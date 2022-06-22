72c7f18cc73ea7bf727a7e31522a53dccd137aa9
BiggusMaximus <annastyabagas164@gmail.com>
2022-06-22 21:15:08 +0700

    Adding wifi function

--- sending_data_from_sensor/sending_data_from_sensor.ino
+++ sending_data_from_sensor/sending_data_from_sensor.ino
@@ -9,15 +9,14 @@
 #include "MAX30105.h"
 #include "heartRate.h"
 
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 32 // OLED display height, in pixels
-#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
-#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
-
+#define SCREEN_WIDTH 128 
+#define SCREEN_HEIGHT 32 
+#define OLED_RESET     -1 
+#define SCREEN_ADDRESS 0x3C 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 MAX30105 particleSensor;
 
-// Adding wifi ssid, password and server name
+
 const char* ssid = "PARAMITA";
 const char* password = "76602223";
 const char* serverName = "http://192.168.0.100/ESP8266_Smartwatch_BPM_Detection/sensorToDatabase.php";
@@ -27,13 +26,13 @@ const long utcOffsetInSeconds = 25200;
 unsigned long timerDelay = 5000;
 unsigned long lastTime = 0;
 
-// Define NTP Client to get time
+
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds);
-const byte RATE_SIZE = 4; //Increase this for more averaging. 4 is good.
-byte rates[RATE_SIZE]; //Array of heart rates
+const byte RATE_SIZE = 4; 
+byte rates[RATE_SIZE]; 
 byte rateSpot = 0;
-long lastBeat = 0; //Time at which the last beat occurred
+long lastBeat = 0; 
 float beatsPerMinute;
 int beatAvg;
 
@@ -43,7 +42,6 @@ int baca_bpm(){
 
   if (checkForBeat(irValue) == true)
   {
-    //We sensed a beat!
     long delta = millis() - lastBeat;
     lastBeat = millis();
 
@@ -51,10 +49,8 @@ int baca_bpm(){
 
     if (beatsPerMinute < 255 && beatsPerMinute > 20)
     {
-      rates[rateSpot++] = (byte)beatsPerMinute; //Store this reading in the array
-      rateSpot %= RATE_SIZE; //Wrap variable
-
-      //Take average of readings
+      rates[rateSpot++] = (byte)beatsPerMinute;
+      rateSpot %= RATE_SIZE; 
       beatAvg = 0;
       for (byte x = 0 ; x < RATE_SIZE ; x++)
         beatAvg += rates[x];
@@ -68,7 +64,6 @@ int baca_bpm(){
 }
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
   WiFi.begin(ssid, password);
   Serial.println("Connecting");
@@ -81,29 +76,27 @@ void setup() {
   Serial.println(WiFi.localIP()); 
   if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
     Serial.println(F("SSD1306 allocation failed"));
-    for(;;); // Don't proceed, loop forever
+    for(;;); 
   }
   display.display();
-  delay(2000); // Pause for 2 seconds
+  delay(2000); 
 
-  // Clear the buffer
   display.clearDisplay();
   timeClient.begin();
-  if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) //Use default I2C port, 400kHz speed
+  if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) 
   {
     Serial.println("MAX30105 was not found. Please check wiring/power. ");
     while (1);
   }
   Serial.println("Place your index finger on the sensor with steady pressure.");
 
-  particleSensor.setup(); //Configure sensor with default settings
-  particleSensor.setPulseAmplitudeRed(0x0A); //Turn Red LED to low to indicate sensor is running
-  particleSensor.setPulseAmplitudeGreen(0); //Turn off Green LED
+  particleSensor.setup(); 
+  particleSensor.setPulseAmplitudeRed(0x0A); 
+  particleSensor.setPulseAmplitudeGreen(0); 
 
 }
 
 void loop() {
-  // Read data from sensor
   display.clearDisplay();
   display.setTextSize(1);
   display.setTextColor(WHITE);
@@ -166,33 +159,15 @@ void loop() {
   display.display(); 
   
   if ((millis() - lastTime) > timerDelay) {
-    //Check WiFi connection status
     if(WiFi.status()== WL_CONNECTED){
       WiFiClient client;
       HTTPClient http;
-      
-      // Your Domain name with URL path or IP address with path
       http.begin(client, serverName);
-
-      // Specify content-type header
       http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-      // Data to send with HTTP POST
       String httpRequestData = "bpm=" + String(baca_bpm()) + "";  
-      // Send HTTP POST request
       int httpResponseCode = http.POST(httpRequestData);
-      
-      // If you need an HTTP request with a content type: application/json, use the following:
-      //http.addHeader("Content-Type", "application/json");
-      //int httpResponseCode = http.POST("{\"api_key\":\"tPmAT5Ab3j7F9\",\"sensor\":\"BME280\",\"value1\":\"24.25\",\"value2\":\"49.54\",\"value3\":\"1005.14\"}");
-
-      // If you need an HTTP request with a content type: text/plain
-      //http.addHeader("Content-Type", "text/plain");
-      //int httpResponseCode = http.POST("Hello, World!");
-     
       Serial.print("HTTP Response code: ");
       Serial.println(httpResponseCode);
-        
-      // Free resources
       http.end();
     }
     else {
