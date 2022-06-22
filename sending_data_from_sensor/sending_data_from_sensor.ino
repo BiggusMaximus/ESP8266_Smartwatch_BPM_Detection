637544e86d26441742b5cbfe9f5d2f1bdb91402b
BiggusMaximus <annastyabagas164@gmail.com>
2022-06-22 13:54:26 +0700

    Adding Display bpm on oled

--- sending_data_from_sensor/sending_data_from_sensor.ino
+++ sending_data_from_sensor/sending_data_from_sensor.ino
@@ -6,12 +6,16 @@
 #include <Adafruit_SSD1306.h>
 #include <WiFiUdp.h>
 #include <NTPClient.h>
+#include "MAX30105.h"
+#include "heartRate.h"
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 32 // OLED display height, in pixels
 #define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
 #define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+MAX30105 particleSensor;
 
 // Adding wifi ssid, password and server name
 const char* ssid = "PARAMITA";
@@ -20,11 +24,47 @@ const char* serverName = "http://192.168.0.100/ESP8266_Smartwatch_BPM_Detection/
 
 
 const long utcOffsetInSeconds = 25200;
-String hari[7] = {"Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jum'at", "Sabtu"};
+
 
 // Define NTP Client to get time
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds);
+const byte RATE_SIZE = 4; //Increase this for more averaging. 4 is good.
+byte rates[RATE_SIZE]; //Array of heart rates
+byte rateSpot = 0;
+long lastBeat = 0; //Time at which the last beat occurred
+float beatsPerMinute;
+int beatAvg;
+
+
+int baca_bpm(){
+  long irValue = particleSensor.getIR();
+
+  if (checkForBeat(irValue) == true)
+  {
+    //We sensed a beat!
+    long delta = millis() - lastBeat;
+    lastBeat = millis();
+
+    beatsPerMinute = 60 / (delta / 1000.0);
+
+    if (beatsPerMinute < 255 && beatsPerMinute > 20)
+    {
+      rates[rateSpot++] = (byte)beatsPerMinute; //Store this reading in the array
+      rateSpot %= RATE_SIZE; //Wrap variable
+
+      //Take average of readings
+      beatAvg = 0;
+      for (byte x = 0 ; x < RATE_SIZE ; x++)
+        beatAvg += rates[x];
+      beatAvg /= RATE_SIZE;
+    }
+  }
+  if (irValue < 50000)
+    Serial.print(" No finger?");
+    
+  return beatAvg;
+}
 
 void setup() {
   // put your setup code here, to run once:
@@ -48,6 +88,17 @@ void setup() {
   // Clear the buffer
   display.clearDisplay();
   timeClient.begin();
+  if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) //Use default I2C port, 400kHz speed
+  {
+    Serial.println("MAX30105 was not found. Please check wiring/power. ");
+    while (1);
+  }
+  Serial.println("Place your index finger on the sensor with steady pressure.");
+
+  particleSensor.setup(); //Configure sensor with default settings
+  particleSensor.setPulseAmplitudeRed(0x0A); //Turn Red LED to low to indicate sensor is running
+  particleSensor.setPulseAmplitudeGreen(0); //Turn off Green LED
+
 }
 
 void loop() {
@@ -106,7 +157,11 @@ void loop() {
   Serial.print(currentYear);
   display.print(currentYear);
   Serial.println();
+ 
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(0,10);
+  display.print("BPM : " + String(baca_bpm()));
   display.display(); 
-  
-  delay(1000);
+
 }
